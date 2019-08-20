3230006127eeb8c3a6c30d590d0b4ef693bd1e7f
Areum Jo <areumjo1@gmail.com>
2019-08-20 11:59:49 -0400

    Project set up and added dependencies

---
+++ friends/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>make-a-room</p>
+        <p>Login</p>
+        <p>Sign-up</p>
+      </header>
+      <p>Welcome to make-a-room</p>
+    </div>
+  );
+}
+
+export default App;
